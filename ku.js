8ca53efa2c42cba22e14c48f9d867aa4415ce320
utilika <116301544+utilika@users.noreply.github.com>
2023-04-21 19:28:39 +0300

    Add files via upload

---
+++ ku.js
@@ -0,0 +1,11 @@
+
+let i = prompt('Password');
+
+if (i === 'giga') {
+    
+    alert('Привет Джяк хара');
+
+    document.getElementById("virus").style.display='none';
+    
+} else {alert('You get virus');}
+
